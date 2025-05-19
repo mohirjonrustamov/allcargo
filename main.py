833df6cac60a1b651ff1c286e6b87dffc1b5d611
mohirjonrustamov <mohirjonrustamov5704@gmail.com>
2025-05-19 10:57:02 +0500

    main.py

--- main.py
+++ main.py
@@ -909,7 +909,7 @@ async def main():
     await runner.setup()
 port = int(os.environ.get("PORT", 8080))
 site = web.TCPSite(runner, "0.0.0.0", port)
-    await site.start()
+await site.start()
 
     print("Bot ishga tushdi...")
     while True:
